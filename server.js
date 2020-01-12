48c65563ff5f2a4cc8cca556b9909bef4425bae6
avijeetpandey25 <avijeet_official@outlook.com>
2020-01-12 17:14:37 +0530

    added server and post logic

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const app=express();
 const bodyParser=require('body-parser');
 const ejs =require('ejs');
 
+//database stuff
+const mongoose=require('mongoose');
+
+//making connection request to the database
+const url="mongodb://localhost:27017/postsDB";
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
+
+
+    
+//new schema for the database
+const postSchema=new mongoose.Schema({
+        title:{
+            type:String,
+            required:true
+        },
+        content:{
+            type:String,
+            required:true
+        }
+    });
+    
+//making model for the database 
+const Post=new mongoose.model('post',postSchema);
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine",'ejs');
@@ -18,6 +42,22 @@ app.get('/about',(req,res)=>{
 
 app.get('/create',(req,res)=>{
     res.render('create');
+})
+
+
+app.post('/create',(req,res)=>{
+
+    let title=req.body.title;
+    let content=req.body.content;
+
+    let post=new Post({
+        title:title,
+        content:content
+    });
+
+
+    post.save();
+
 })
 
 app.listen(3000,()=>{
